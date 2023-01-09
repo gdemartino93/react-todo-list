e2109ba650a4c3f9a60588a55d07b1c3144540da
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-09 18:21:31 +0100

    remove thing to do by clicking

--- src/AppMain.js
+++ src/AppMain.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import { toDoArray} from "./data";
-import Thing from './Thing';
-const ArrayState = () => {
-    const [toDo , setToDo] = useState(toDoArray);
-}
+
 
 const AppMain = () => {
+    const [toDo, setToDo] = useState(toDoArray);
+    const removeThing = (clicked) =>{
+        let removeToDo = toDo.filter( thing => thing.id !== clicked);
+        setToDo(removeToDo)
+    }
   return (
     <section>
         {
-            toDoArray.map( thing =>{
-               return <Thing key={thing.id} {...thing} />
+            toDo.map( thing =>{
+               return <div key={thing.id}  className="thing" >
+                <span>{thing.name}</span>
+                <button onClick={()=> removeThing(thing.id)}>X</button>
+                </div>
             })
         }
     </section>
--- src/data.js
+++ src/data.js
@@ -2,6 +2,7 @@ export const toDoArray = [
     { id: 1, name: "fare la spesa" },
     { id: 2, name: "andare in palestra" },
     { id: 3, name: "fare il sito" },
+    { id : 4, name: "fare il bucato"}
 
   ];
   
